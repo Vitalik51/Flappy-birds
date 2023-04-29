34d49f29f00b4487915fd16870319fb647df53e4
Mishutkaa <boarcukmihajlo@gmail.com>
2023-04-29 15:31:21 +0300

    four lesson

--- main.py
+++ main.py
@@ -47,28 +47,31 @@ class Obstacles(GameSprite):
         self.player = player
 
     def update(self):
-        self.rect.x -= self.player.speed
+        self.rect.x -= 3
 
 
 
 bird = Player('bird1.png', 200, 200, 2, 50, 50)
-tryba = Obstacles("tryba1.png", 1200, randint(200, win_width - 800), 200, 200, bird)
-tryban2 = Obstacles("tryba2.png", 1200,  randint(0, win_width + 40), 200, 200, bird)
+tryba = Obstacles("tryba1.png", 1200, 453, 200, 200, bird)
+tryban2 = Obstacles("tryba2.png", 1200, 0, 200, 200, bird)
+tryban3 = Obstacles("tryba1.png", 2000, 453, 200, 200, bird)
+tryban4 = Obstacles("tryba2.png", 2000, 0, 200, 200, bird)
+trigger_point = Obstacles("block.png", 1360, 230, 100, 200, bird)
+trigger_point2 = Obstacles("block.png", 2160, 230, 100, 200, bird)
+
+
 
 font.init()
-score_text = font.SysFont("Arial", 20)
+score_text = font.SysFont("Arial", 50)
 point_x = 0
-lost_text = font.SysFont('Arial', 20)
-point_y = 0
+lost_text = font.SysFont('Arial', 100)
 
-ground_scroll = 0
-scroll_speed = 4
 
 while run:
     window.blit(background, (0, 0))
 
     xy = score_text.render(str(point_x), 1, (0,0,0))
-    window.blit(xy, (10, 20))
+    window.blit(xy, (550, 25))
 
 
     for e in event.get():
@@ -76,7 +79,7 @@ while run:
            run = False
    
     
-
+    
 
     if finish == False:
         bird.reset()
@@ -87,11 +90,41 @@ while run:
 
         tryban2.reset()
         tryban2.update()
-        
-        if sprite.collide_rect(tryba, bird) or sprite.collide_rect(tryban2, bird) or bird.rect.x >= 400:
-            finish = True
-            
 
+        tryban3.reset()
+        tryban3.update()
+
+        tryban4.reset()
+        tryban4.update()
+
+        trigger_point.update()
+
+        trigger_point2.update()
+
+
+        if tryba.rect.x < 0:
+            tryba.rect.x += 1800 
+        if tryban2.rect.x < 0:
+            tryban2.rect.x += 1800   #1 труба
+        if tryban3.rect.x < 0:
+            tryban3.rect.x += 1800
+        if tryban4.rect.x < 0:
+            tryban4.rect.x += 1800 #2 труба 
+        if trigger_point.rect.x < 0:
+            trigger_point.rect.x += 1800
+        if trigger_point2.rect.x < 0:
+            trigger_point2.rect.x += 1800 
+
+
+        if sprite.collide_rect(bird, trigger_point) or sprite.collide_rect(bird, trigger_point2):
+            point_x += 0.021
+    if sprite.collide_rect(tryba, bird) or sprite.collide_rect(tryban2, bird) or sprite.collide_rect(tryban3, bird) or sprite.collide_rect(tryban4, bird) or bird.rect.y >= 600:
+        finish = True
+        text5 = lost_text.render("Ви програли", 1, (0, 0, 0))
+        window.blit(text5, (350, 250))
+            
+    if score_text == 25:
+        finish = True
 
 
 
